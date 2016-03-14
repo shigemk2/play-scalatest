daef8616b3704c4c93f07e6a81999c5cf69a32eb
shigemk2 <i.am.shige@gmail.com>
2016-03-14 20:25:23 +0900

    Change should to must

--- test/TestSpec.scala
+++ test/TestSpec.scala
@@ -1,14 +1,14 @@
 import collection.mutable.Stack
 import org.scalatest._
 
-class ExampleSpec extends FlatSpec with Matchers {
+class ExampleSpec extends FlatSpec with MustMatchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
     stack.push(1)
     stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    stack.pop() mustBe (2)
+    stack.pop() mustBe (1)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
